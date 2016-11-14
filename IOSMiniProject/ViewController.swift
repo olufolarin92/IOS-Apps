ac6ab0e74af50c8edda54044e13a271dc55f03bf
Olufolarin Bajomo <ob218@lonerangers-mbp.home>
2016-11-14 15:38:44 +0000

    Initial Commit

---
+++ IOSMiniProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IOSMiniProject
+//
+//  Created by Olufolarin Bajomo on 14/11/2016.
+//  Copyright © 2016 Olufolarin Bajomo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
